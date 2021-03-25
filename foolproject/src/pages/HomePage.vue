b0d39f6228fa7a0310a3a6a2fa5c457a7052cee0
Nic <nic.goodman@hey.com>
2021-03-24 23:00:21 -0400

    Testing Filtering Articles by Tag
    Created a computed property filtering articles containing 10-promise tag

--- foolproject/src/pages/HomePage.vue
+++ foolproject/src/pages/HomePage.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="" class="">
-      <ul>
-          <li v-for="article in articles" v-bind:key="article.uuid">
-              {{ article.headline }}
-          </li>
-      </ul>
+    <ul>
+      <li v-for="article in articles" v-bind:key="article.uuid">
+        <h1>
+          {{ article.headline }}
+        </h1>
+      </li>
+      Latest
+      <li>
+        <h1>
+          {{ latestArticles[0].headline }}
+        </h1>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   name: "HomePage",
@@ -18,13 +26,19 @@ export default {
       articles: [],
     };
   },
+  computed: {
+    latestArticles: function () {
+      return this.articles.filter((article) => {
+        return article.tags.some((tag) => tag.slug.includes("10-promise"));
+      });
+    },
+  },
   mounted() {
     axios
       .get("http://127.0.0.1:8000/content")
       .then((response) => (this.articles = response.data.results));
   },
   props: {},
-  components: {
-  },
+  components: {},
 };
 </script>
