df70be1c4158a1b0ae1aa2ec9985c09ea83f9125
Nic <nic.goodman@hey.com>
2021-03-24 22:06:33 -0400

    Installed Axios and performed initial API test
    Installed Axios and populated an array with the articles from the feed.

--- foolproject/src/main.js
+++ foolproject/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import './assets/styles/index.css';
+import axios from 'axios';
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
 }).$mount('#app');
+
+Vue.use(axios);
--- foolproject/src/pages/HomePage.vue
+++ foolproject/src/pages/HomePage.vue
@@ -1,15 +1,30 @@
-<template></template>
+<template>
+  <div id="" class="">
+      <ul>
+          <li v-for="article in articles" v-bind:key="article.uuid">
+              {{ article.headline }}
+          </li>
+      </ul>
+  </div>
+</template>
 
 <script>
-import ArticleCard from "./components/ArticleCard.vue";
-import LatestCard from "./components/LatestCard.vue";
+import axios from 'axios';
 
 export default {
   name: "HomePage",
+  data() {
+    return {
+      articles: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("http://127.0.0.1:8000/content")
+      .then((response) => (this.articles = response.data.results));
+  },
   props: {},
   components: {
-    ArticleCard,
-    LatestCard,
   },
 };
 </script>
