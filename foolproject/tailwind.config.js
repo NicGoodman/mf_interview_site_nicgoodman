4d30ad45c7df13b52a563f3371922628be2979c6
Nic <nic.goodman@hey.com>
2021-03-28 18:26:23 -0400

    Added comments to tailwind and index page

--- foolproject/tailwind.config.js
+++ foolproject/tailwind.config.js
@@ -3,9 +3,11 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      //adding the futura font family to the tailwind config
       fontFamily: {
         futura: "futura-pt, sans-serif",
       },
+      //these are the MF colors used in the project
       colors: {
         "mf-gold": "#ffb81c",
         "mf-red": "#f9423a",
@@ -19,6 +21,7 @@ module.exports = {
   variants: {
     extend: {},
   },
+  //activates the 3rd party text shadow plugin to easily add shadows to text elements.
   plugins: [
     require('tailwindcss-textshadow')
   ],
